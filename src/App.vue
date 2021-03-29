4596402fb7c5d29d67c1e92bec35b637febfe88d
ziga <zigomir@gmail.com>
2021-03-29 15:19:17 +0200

    Repro

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div v-for="(value, key) in obj">
+      <span>{{key}}</span>: {{ value.toFixed() }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const obj: Record<'key1' | 'key2', number> = {
+      key1: 1,
+      key2: 2,
+    }
+
+    return { obj }
+  }
+})
+</script>
